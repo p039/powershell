696424cd1593f70be85a08685f9818e9f9cb806e
p039 <pavel.odnodvortsev@live.com>
2016-03-10 00:56:44 +0200

    Terminate python processes with sorting and exclusion.
    Added powershell script which terminates python processes.

---
+++ TerminateProcess.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.SYNOPSIS
+    1. Sort python processes by StartDate and get the list excluding first 3 (the running app).
+    2. Kill python processes which are running longer than 5 minutes.
+#>
+
+$processObjects = Get-Process python | Sort {$_.StartTime}  | Select -Skip 3
+
+foreach ($proc in $processObjects)
+ {
+    if ($proc.StartTime -lt (Get-Date).AddMinutes(-5))
+    {
+        $proc.Kill()
+    }
+}
