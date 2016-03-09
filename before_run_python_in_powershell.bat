3bae865e995e138cc1fc243c7e12a8addf3e2562
p039 <pavel.odnodvortsev@live.com>
2016-03-10 01:24:36 +0200

    Refactoring

--- before_run_python_in_powershell.bat
+++ before_run_python_in_powershell.bat
@@ -1,3 +1,3 @@
-﻿REM :: Run the script and restart powersell console.
+﻿REM :: Run the script and restart powershell console.
 
 [Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Python27","User")
