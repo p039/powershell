94acf0be4ed52561cc17ddd8a25a4518197e2bc5
p039 <pavel.odnodvortsev@live.com>
2016-03-10 01:11:28 +0200

    Bat file to run python in PS

---
+++ run_python_in_ps.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+
+cls
+
+set ROOT_DIR=C:\
+set APP_DIR=%ROOT_DIR%\app\bin
+
+
+
+c:
+cd \
+cd %APP_DIR%
+
+start PowerShell.exe python backend.py
+timeout 5
+start PowerShell.exe python frontend.py
